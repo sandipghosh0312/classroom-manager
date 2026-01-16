2d98f4c3f07970d23a56b3e9370dfaea089b058a
Sandip <sandipghosh0312@gmail.com>
2026-01-16 15:47:10 +0530

    Implement rest data provider

--- src/pages/subjects/SubjectList.tsx
+++ src/pages/subjects/SubjectList.tsx
@@ -36,7 +36,7 @@ const SubjectList = () => {
             },
             {
                 id: 'department',
-                accessorKey: 'department',
+                accessorKey: 'department.name',
                 size: 150,
                 header: () => <p className='column-title ml-2'>Department</p>,
                 cell: ({ getValue }) => <Badge variant="secondary">{getValue<string>()}</Badge>
--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,36 +1,40 @@
-import { BaseRecord, DataProvider, GetListParams, GetListResponse } from "@refinedev/core";
-import { mockSubjects } from "../constants/mock-data";
+import { BACKEND_BASE_URL } from "@/constants";
+import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
+import { ListResponse } from "@/types";
 
-export const dataProvider: DataProvider = {
-  getList: async <TData extends BaseRecord = BaseRecord>(
-    { resource }: GetListParams
-  ): Promise<GetListResponse<TData>> => {
+const options: CreateDataProviderOptions = {
+  getList: {
+    getEndpoint: ({ resource }) => resource,
+    buildQueryParams: async ({resource, pagination, filters}) => {
+      const page = pagination?.currentPage ?? 1;
+      const pageSize = pagination?.pageSize ?? 10;
 
-    if (resource !== "subjects") {
-      return {
-        data: [] as TData[],
-        total: 0,
-      };
+      const params: Record<string, string|number> = {page, limit: pageSize};
+
+      filters?.forEach((filter) => {
+        const field = 'field' in filter ? filter.field : '';
+
+        const value = String(filter.value);
+
+        if (resource === 'subjects') {
+          if (field === 'department') params.department = value;
+          if (field === 'name' || field === 'code') params.search = value;
+        }
+      })
+
+      return params;
+    },
+    mapResponse: async (response) => {
+      const payload: ListResponse = await response.json();
+      return payload.data ?? [];
+    },
+    getTotalCount: async (response) => {
+      const payload: ListResponse = await response.json();
+      return payload.pagination?.total ?? payload.data?.length ?? 0;
     }
+  }
+}
 
-    return {
-      data: mockSubjects as unknown as TData[],
-      total: mockSubjects.length,
-    };
-  },
+const { dataProvider } = createDataProvider(BACKEND_BASE_URL, options);
 
-  getOne: async () => {
-    throw new Error("This function is not present in mock.");
-  },
-  create: async () => {
-    throw new Error("This function is not present in mock.");
-  },
-  update: async () => {
-    throw new Error("This function is not present in mock.");
-  },
-  deleteOne: async () => {
-    throw new Error("This function is not present in mock.");
-  },
-
-  getApiUrl: () => "",
-};
+export { dataProvider };
