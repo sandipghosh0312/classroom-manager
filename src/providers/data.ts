3113fcb7a0570543c2b14ba5bb2493f69fd2ed97
Sandip <sandipghosh0312@gmail.com>
2026-01-17 18:11:31 +0530

    Implement the rate limit and security features like bot detection and and shield protection

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,9 +1,28 @@
 import { BACKEND_BASE_URL } from "@/constants";
 import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
 import { ListResponse } from "@/types";
+import { HttpError } from "@refinedev/core";
 
 if (!BACKEND_BASE_URL) {
   throw new Error("BACKEND_BASE_URL is not defined");
+}
+
+const buildHttpError = async (response: Response): Promise<HttpError> => {
+  let message = 'Request failed';
+
+
+  try {
+    const payload = (await response.json()) as { message?: string };
+
+    if (payload?.message) message = payload.message;
+  } catch (error) {
+    // Ignore errors
+  }
+
+  return {
+    message,
+    statusCode: response.status,
+  }
 }
 
 const options: CreateDataProviderOptions = {
@@ -29,10 +48,16 @@ const options: CreateDataProviderOptions = {
       return params;
     },
     mapResponse: async (response) => {
+      if (!response.ok) {
+        throw await buildHttpError(response);
+      }
       const payload: ListResponse = await response.clone().json();
       return payload.data ?? [];
     },
     getTotalCount: async (response) => {
+      if (!response.ok) {
+        throw await buildHttpError(response);
+      }
       const payload: ListResponse = await response.clone().json();
       return payload.pagination?.total ?? payload.data?.length ?? 0;
     }
